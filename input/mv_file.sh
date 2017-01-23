4eb98b7e984e881e09ec629970f12fe0ffaf4270
V97ug <s1220164take@gmail.com>
2017-01-23 15:56:22 +0900

    enable to process parameter

--- input/mv_file.sh
+++ input/mv_file.sh
@@ -2,17 +2,40 @@
 
 ###
 # mv_file.sh
-# posとnegにあるファイルの一部を、testPosとtestegに分配する
 ###
 
-readonly NUM_MOVE_FILES=50
+function usage() {
+cat <<_EOT_
+Usage:
+  no parameter: num_pos=50, num_neg=50
+   \$1: num_pos=\$1, num_neg=\$1
+   \$1, \$2: num_pos=\$1, num_neg=\$2
 
-for file in `ls -d pos/* | head -n ${NUM_MOVE_FILES}`
+Description:
+  posとnegにあるファイルの一部を、testPosとtestegに分配する
+_EOT_
+exit 1
+}
+
+num_pos_move_files=50
+num_neg_move_files=50
+
+if [ $# -eq 1 ]; then
+  num_pos_move_files=$1
+  num_neg_move_files=$1
+fi
+
+if [ $# -eq 2 ]; then
+  num_pos_move_files=$1
+  num_neg_move_files=$2
+fi
+
+for file in `ls -d pos/* | head -n ${num_pos_move_files}`
 do
   mv ${file} testPos/
 done
 
-for file in `ls -d neg/* | head -n ${NUM_MOVE_FILES}`
+for file in `ls -d neg/* | head -n ${num_neg_move_files}`
 do
   mv ${file} testNeg/
 done
